ef34d8c3eb8b4a87444d6c950cd406b7dab0a8d9
Bryan-Cardenas1 <bryan.cardenas@alumnos.upla.cl>
2024-12-07 04:24:51 -0300

    Posible formato de presentacion

---
+++ resultados.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poke Wiki - Pokemones</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body class="bg-dark text-white">
+<main>
+        <div class="container-fluid d-flex justify-content-center align-items-center">
+            <div class="row mt-5">
+                <header class="col-12 text-center">
+                    <h3>Poke Wiki - Pokemones</h3>
+                </header>
+
+                <div class="col-md-4 mb-3">
+                    <div class="card bg-secondary text-white">
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" class="card-img-top" alt="Bulbasaur">
+                        <div class="card-body">
+                            <h5 class="card-title">Bulbasaur</h5>
+                            <p class="card-text">Tipo: Planta/Veneno</p>
+                            <a href="pokemon-detail.html?id=1" class="btn btn-danger">Ver detalles</a>
+                        </div>
+                    </div>
+                </div>
+
+               
+                <div class="col-md-4 mb-3">
+                    <div class="card bg-secondary text-white">
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png" class="card-img-top" alt="Ivysaur">
+                        <div class="card-body">
+                            <h5 class="card-title">Ivysaur</h5>
+                            <p class="card-text">Tipo: Planta/Veneno</p>
+                            <a href="pokemon-detail.html?id=2" class="btn btn-danger">Ver detalles</a>
+                        </div>
+                    </div>
+                </div>
+
+            
+                <div class="col-md-4 mb-3">
+                    <div class="card bg-secondary text-white">
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" class="card-img-top" alt="Venusaur">
+                        <div class="card-body">
+                            <h5 class="card-title">Venusaur</h5>
+                            <p class="card-text">Tipo: Planta/Veneno</p>
+                            <a href="pokemon-detail.html?id=3" class="btn btn-danger">Ver detalles</a>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+    </main>
+
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
