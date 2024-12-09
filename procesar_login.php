dfe26dbfa57ab723c427ce64726e0f39ba39e27a
Xeno <131300185+Diego-Ulpin12@users.noreply.github.com>
2024-12-09 15:53:55 -0300

    Create procesar_login.php
    Procesar los datos ingresados por login.php

---
+++ procesar_login.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once "conexion.php";
+require "login.php";
+
+$email = $_POST["email"];
+$contraseña = $_POST["contraseña"];
+
+function consultar($conn, $email, $contraseña)
+{
+    $sql = "SELECT email, contraseña 
+            FROM registro 
+            WHERE $email = $email AND $contraseña = $contraseña";
+    $resultado = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($resultado) > 0)
+    {
+        echo "Bienvenido usuario";
+    }else{
+        echo "El usuario ingresado no existe. Por favor registrese primero";
+    }
+}
+
+consultar
+(
+    $conn,
+    $email,
+    $contraseña,
+);
