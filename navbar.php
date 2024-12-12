edfc88aaeb53f0c714386d10bc4e5c9f71d3faf8
Bryan-Cardenas1 <bryan.cardenas@alumnos.upla.cl>
2024-12-12 12:41:47 -0300

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Mi PokeWiki</a>
+      <a class="navbar-brand" href="index.php">Mi PokeWiki</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -14,14 +14,9 @@
             <a class="nav-link active" aria-current="page" href="login.php">Iniciar sesion</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Pokemones</a>
+            <a class="nav-link" href="resultados.php">PokeDex</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Acerca de</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contacto</a>
-          </li>
+        
         </ul>
 
       </div>
