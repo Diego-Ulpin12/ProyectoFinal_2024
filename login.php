318124c0182df78f434afdc2946fb29b15125554
Bryan-Cardenas1 <bryan.cardenas@alumnos.upla.cl>
2024-12-12 04:41:14 -0300

    fix

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
   <body class="bg-dark text-white">
   <main class="vh-100">
   <?php require_once "navbar.php" ?>
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
             <div class="col-12">
                 <h1>Inicio de sesion</h1>
